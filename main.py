487404d1427a4b2c047f6dab81e8281b05abfaa4
Jorge Presti <jorge.vazquez98@uabc.edu.mx>
2024-05-22 08:13:32 -0700

    blackjack table add

---
+++ main.py
@@ -0,0 +1,22 @@
+import pgzrun
+
+# Window size (background image size DON'T CHANGE)
+WIDTH = 1286
+HEIGHT = 772
+
+
+def draw():
+    screen.clear()
+    screen.blit('blackjack_fondo', (0, 0))
+
+
+
+
+
+
+
+
+
+
+
+pgzrun.go()
