19a618b75ca409410c3aaca6a1050629b8003db2
JVPresti <nanojorgevazquez123@gmail.com>
2024-05-22 19:31:04 -0700

    some validation of AS

---
+++ TODO.md
@@ -0,0 +1,19 @@
+1. Hacer la logica del juego
+    [X] - El juego reparte 2 cartas a cada jugador incluida la casa
+    [ ] - El juego es capaz de notar cuando la casa gana y automaticamente termina la ronda
+    [ ] - El juego puede dejar pedir otra carta 
+    [ ] - EL juego debe decidir si perdiste con la suma de tus cartas
+    [X] - Si la casa tiene 16 en su suma, tiene que pedir otra carta
+        [X] -  Si la casa tiene 17 o mas se puede quedar con eso
+    [X] - Validar que el sistema identifiquen el AS como 1 u 11
+        [ ] - Validar que el usuario identifique el AS como 1 u 11
+        [X] - Si al momento de repartir toca 2 AS (11) uno se cambia por 1 automaticamente
+    
+2. Hacer que se muestren las cartas que le toco a cada jugador
+    [ ] - Con las variables FCPLAYER y SCPLAYER se tiene que tomar la carta de images
+        [ ] - Seleccionar un palo de forma aleatoria
+    [ ] - La casa debe mostrar solo 1 carta, la otra tiene que ser oculta
+        [ ] - La casa muestra sus cartas cuando terminan las apuestas
+
+3. Hacer las fichas de las apuestas
+    [ ] - Se debe poner de alguna manera controlar las apuestas
--- main.py
+++ main.py
@@ -26,7 +26,12 @@ SCPLAYER3 = 0
 SCHOUSE = 0
 
 # Game state
-GAME_STATE = 1  # 1 = Playing, 0 = House wins
+
+# 1 = Playing, 0 = House wins, 3 = House wins with 21 or in the first play
+GAME_STATE_HOUSE = 1
+GAME_STATE_PLAYER1 = 1
+GAME_STATE_PLAYER2 = 1
+GAME_STATE_PLAYER3 = 1
 
 
 #! Load the images
@@ -49,8 +54,6 @@ resized_image_surface.blit(resized_image, (0, 0))
 
 def draw():
     logic()
-    print("First card player 1 ", FCPLAYER1)
-    print("First card player 2 ", FCPLAYER2)
     screen.clear()
     screen.blit('blackjack_fondo', (0, 0))
     screen.blit(resized_image_surface, PLAYER1_IMAGE_POS)
@@ -58,8 +61,9 @@ def draw():
 
 # Logic of the game
 def logic():
-    global GAME_STATE, PLAYER1, PLAYER2, PLAYER3, HOUSE, FCPLAYER1, FCPLAYER2, FCPLAYER3, FCHOUSE, SCPLAYER1, SCPLAYER2, SCPLAYER3, SCHOUSE
+    global GAME_STATE_HOUSE, GAME_STATE_PLAYER1, GAME_STATE_PLAYER2, GAME_STATE_PLAYER3, PLAYER1, PLAYER2, PLAYER3, HOUSE, FCPLAYER1, FCPLAYER2, FCPLAYER3, FCHOUSE, SCPLAYER1, SCPLAYER2, SCPLAYER3, SCHOUSE
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    firstPlay = 0  # This is to control the first play of the house
 
     # Asign cards to players and house
     for i in range(2):
@@ -67,7 +71,23 @@ def logic():
         PLAYER2.append((random.choice(numbers)))
         PLAYER3.append((random.choice(numbers)))
         HOUSE.append((random.choice(numbers)))
+
+        # This is to change the value of the card 11 to 1. Only if the player or house has two 11 cards
+        if (HOUSE.count(11) == 2):
+            HOUSE.remove(11)
+            HOUSE.append(1)
+        if (PLAYER1.count(11) == 2):
+            PLAYER1.remove(11)
+            PLAYER1.append(1)
+        if (PLAYER2.count(11) == 2):
+            PLAYER2.remove(11)
+            PLAYER2.append(1)
+        if (PLAYER3.count(11) == 2):
+            PLAYER3.remove(11)
+            PLAYER3.append(1)
+
         if i == 0:
+            # Variables to take the cards of the images folder
             FCPLAYER1 = PLAYER1[i]
             FCPLAYER2 = PLAYER2[i]
             FCPLAYER3 = PLAYER3[i]
@@ -78,6 +98,22 @@ def logic():
             SCPLAYER3 = PLAYER3[i]
             SCHOUSE = HOUSE[i]
 
+    while (GAME_STATE_HOUSE == 1):
+        # This is to control the cards taken by the house
+        if (firstPlay == 0 and sum(HOUSE) == 21):
+            GAME_STATE_HOUSE = 3
+        if (sum(HOUSE) < 17):
+            HOUSE.append((random.choice(numbers)))
+            # This is to change the value of the card 11 to 1. Only if the player or house has two 11 cards
+            if (HOUSE.count(11) == 2):
+                HOUSE.remove(11)
+                HOUSE.append(1)
+        if (sum(HOUSE) > 21 and HOUSE.count(11) < 2):
+            GAME_STATE_HOUSE = 0
+        if (sum(HOUSE) > 16 and sum(HOUSE) < 22):
+            GAME_STATE_HOUSE = 0
+
+    #! Esto es para hacer debug
     print("Cards player 1 ", PLAYER1)
     print("Cards player 2 ", PLAYER2)
     print("Cards player 3 ", PLAYER3)
