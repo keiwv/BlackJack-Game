a889eb3675cb186e45ceb2ba76b9f0e31276e27f
JVPresti <nanojorgevazquez123@gmail.com>
2024-11-04 12:15:30 -0800

    pos added

--- src/cImages.py
+++ src/cImages.py
@@ -1,4 +1,12 @@
+import pygame
+import pgzrun
+import os
+
 class Images:
     def __init__(self):
-        reloadButton = Actor("./images/reload.png")
-        chip25 = Actor("./images/chips/ficha25.png")
+        reloadButton = Actor("./images/reload.png", pos=(1180,118))
+        chip25 = Actor("./images/chips/ficha25.png", pos=(60,200))
+        chip50 = Actor("./images/chips/ficha50.png", pos=(60, 280))
+        chip100 = Actor("./images/chips/ficha100.png", pos=(60, 360))
+        chip500 = Actor("./images/chips/ficha500.png", pos=(60, 440))
+        background = Actor("./images/background/blackjack_fondo.png")
